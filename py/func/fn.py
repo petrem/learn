ac3b8e4d170ad8e728ca89c0299eba14ed5ce2c8
Petre Mierlutiu <petrem@users.noreply.github.com>
2018-06-03 14:25:12 +0300

    pep8

--- py/func/fn.py
+++ py/func/fn.py
@@ -1,39 +1,46 @@
-import itertools
-
-
 def iffun(cond, a1, a2):
     ''' call like iffun(cond, lambda: f1(), lambda: f2())'''
     return a1() if cond else a2()
 
 
-### Fold
+# ## Fold
+
 
 def foldl(op, initial, seq):
     return op(foldl(op, initial, seq[1:]), seq[0]) if seq else initial
+
 
 def foldr(op, initial, seq):
     return op(seq[-1], foldr(op, initial, seq[:-1])) if seq else initial
 
 
-### Lists
+# ## Lists
+
 
 def head(L):
     return L[0] if L else None
 
+
 def tail(L):
     return L[1:]
+
 
 def construct(h, T):
     return [h] + T
 
+
 def concat(L1, L2):
     return L1 + L2
+
 
 def flatten(L):
     pass
 
+
 def element_at(L, index):
-    return iffun(index == 0, lambda: head(L), lambda: element_at(tail(L), index - 1))
+    return iffun(
+        index == 0, lambda: head(L), lambda: element_at(tail(L), index - 1))
+
 
 def reverse(L):
     return concat(reverse(tail(L)), construct(head(L), [])) if L else []
@@ -51,13 +58,8 @@ def quicksort(L):
         quicksort(filter(lambda x: x >= h, T))
     )
 
-### Generator-based list operations (?)
 
-# Todo
-
-
-
-### Currying
+# ## Currying
 
 # Coded by Massimiliano Tomassoli, 2012.
 # https://mtomassoli.wordpress.com/2012/03/18/currying-in-python/
@@ -68,8 +70,9 @@ def quicksort(L):
 #     - "minArgs = None" is better than "minArgs = -1",
 #     - "if args" is better than "if len(args)", and
 #     - I should use "isdisjoint".
-#
-def genCur(func, unique = True, minArgs = None):
+
+
+def genCur(func, unique=True, minArgs=None):
     """ Generates a 'curried' version of a function. """
     def g(*myArgs, **myKwArgs):
         def f(*args, **kwArgs):
@@ -86,7 +89,8 @@ def genCur(func, unique = True, minArgs = None):
                 newKwArgs.update(kwArgs)
 
                 # Checks whether it's time to evaluate func.
-                if minArgs is not None and minArgs <= len(newArgs) + len(newKwArgs):
+                if (minArgs is not None and
+                        minArgs <= len(newArgs) + len(newKwArgs)):
                     return func(*newArgs, **newKwArgs)  # time to evaluate func
                 else:
                     return g(*newArgs, **newKwArgs)     # returns a new 'f'
@@ -96,9 +100,9 @@ def genCur(func, unique = True, minArgs = None):
     return g
 
 
-def cur(f, minArgs = None):
+def cur(f, minArgs=None):
     return genCur(f, True, minArgs)
 
 
-def curr(f, minArgs = None):
+def curr(f, minArgs=None):
     return genCur(f, False, minArgs)
--- py/prime/prime.py
+++ py/prime/prime.py
@@ -1,12 +1,15 @@
+#!/usr/bin/env python3
 import math
 import numpy
+
 
 def primes(limit):
     primes = numpy.arange(3, limit + 1, 2)
     isprime = numpy.ones((limit - 1)/2, dtype=bool)
     for factor in primes[:int(math.sqrt(limit))]:
         if isprime[factor - 2]:
-            isprime[factor*2-2::factor]=0
+            isprime[factor*2-2::factor] = 0
     return primes[isprime]
 
-print primes(1000000000000066600000000000001)
+
+print(primes(1000000000000066600000000000001))
