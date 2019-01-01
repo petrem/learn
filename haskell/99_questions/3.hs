a7a91d37525204017c4d9d3aebd4d0e4be2c116f
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-01-01 15:48:51 +0200

    add solution to be understood

--- haskell/99_questions/3.hs
+++ haskell/99_questions/3.hs
@@ -9,3 +9,9 @@ elementAt1 k (x:xs) = if k == 1 then x else elementAt1 (k-1) xs
 
 elementAt2 :: Int -> [a] -> a
 elementAt2 k xs = fst $ head $ filter snd $ zipWith (\x y -> (x, y == k)) xs [1..]
+
+
+-- the following, copied from solutions, I do not understand:
+
+elementAt''' xs n = head $ foldr ($) xs
+                         $ replicate (n - 1) tail
