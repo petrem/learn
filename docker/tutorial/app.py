dde44e5a3222864ae7ced14fd628c4b942b9a4bf
Petre Mierlutiu <pmierlutiu@cmedtechnology.com>
2019-01-03 14:55:23 +0200

    minor updates to docker tutorial

--- docker/tutorial/app.py
+++ docker/tutorial/app.py
@@ -1,7 +1,8 @@
-from flask import Flask
-from redis import Redis, RedisError
 import os
 import socket
+
+from flask import Flask
+from redis import Redis, RedisError
 
 # Connect to Redis
 redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
