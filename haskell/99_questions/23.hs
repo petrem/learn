071145c6adfeb23ceefe79d7bf21e5fdc3e092a3
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-03-03 16:34:40 +0200

    haskell 99 questions: 23

--- haskell/99_questions/23.hs
+++ haskell/99_questions/23.hs
@@ -13,6 +13,42 @@ deleteIndexes is xs = map snd $ filter fst $ zip markers xs
   where markers = [if i `elem` is then False else True |i <- [0..]]
 
 
+selectIndexes :: [Int] -> [a] -> [a]
+selectIndexes is xs = (map snd) . (filter fst) $ zip markers xs
+  where markers = [if i `elem` is then True else False |i <- [0..]]
+
+
+deleteAt :: Int -> [a] -> [a]
+deleteAt _ [] = []
+deleteAt k xs = (++) <$> take k <*> drop (k+1) $ xs
+
+
+maybeIndex :: Int -> [a] -> Maybe a
+maybeIndex _ [] = Nothing
+maybeIndex k xs
+  | k < 0 = Nothing
+  | k >= length xs = Nothing
+  | otherwise = Just (xs !! k)
+
+
+extractAt :: Int -> [a] -> (Maybe a, [a])
+extractAt = curry $ (,) <$> uncurry maybeIndex <*> uncurry deleteAt
+
+
+_rnd_select _ 0 _ rs = rs
+_rnd_select gen n xs rs =
+  case extraction of Nothing  -> rs
+                     (Just x) -> _rnd_select newGen (n - 1) remaining (x:rs)
+  where
+    (rnd, newGen) = randomR (0, max 0 (length xs - 1)) gen
+    (extraction, remaining) = extractAt rnd xs
+
+rnd_select n xs = do
+  gen <- newStdGen
+  return $ _rnd_select gen n xs []
+
+
+
 strToInt :: String -> Int
 strToInt s = read s :: Int
 
@@ -22,9 +58,16 @@ parseArgs (n:xs) = (strToInt n, xs)
 parseArgs _ = errorWithoutStackTrace "No arguments. Giving up (cowardly)."
 
 
-main = do
+main1 = do
+  -- select with repetition (?)
   (how_many, list) <- parseArgs <$> getArgs
   gen <- getStdGen
-  let randomIndexes = take how_many $ randomRs (0, length list - 1) gen :: [Int]
-  let result = deleteIndexes randomIndexes list
+  let randomIndexes = take how_many $ randomRs (0, max 0 (length list - 1)) gen :: [Int]
+  let result = selectIndexes randomIndexes list
   putStrLn $ show (how_many, randomIndexes, result)
+
+
+main = do
+    (how_many, list) <- parseArgs <$> getArgs
+    result <- rnd_select how_many list
+    putStrLn $ show result
