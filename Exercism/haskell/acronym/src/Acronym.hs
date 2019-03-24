701537ccb0b3d5e691fbb54d49a3cf1665f72e98
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-03-24 22:12:38 +0200

    exercism: haskell: acrynom v.2 with Data.Text

--- Exercism/haskell/acronym/src/Acronym.hs
+++ Exercism/haskell/acronym/src/Acronym.hs
@@ -1,34 +1,15 @@
 module Acronym (abbreviate) where
 
-
+{-# LANGUAGE OverloadedStrings #-}
+import Data.Text (Text)
+import qualified Data.Text as T
 import qualified Data.Char as C
-import qualified Data.List as L
--- import qualified Data.List.Split as S
 
 
--- abbreviate :: String -> String
--- abbreviate = concat . (map acronymize) . (map (filter C.isAlpha)) . words . toUpperFirst
---   where acronymize = (map head) . S.split (S.condense . S.dropDelims . S.dropBlanks $ S.whenElt C.isLower)
---         toUpperFirst = (:) <$> C.toUpper . head <*> tail
-
--- abbreviate :: String -> String
--- abbreviate = concat . (map acronymize) . words
---   where acronymize = (++) <$> (:[]) . C.toUpper . head <*> (filter C.isUpper) . tail
-
-toUpperHead :: String -> String
-toUpperHead = (:) <$> C.toUpper . head <*> tail
-
-isAllCaps :: String -> Bool
-isAllCaps = all C.isUpper
-
--- unclear case: "GNULinux" should become "G", "GL", what?
-abbreviate :: String -> String
-abbreviate = filter C.isUpper . L.concatMap (toUpperHead . abbrevAllCaps) . words . cleanUp
-  where
-    cleanUp = foldr f []
-      where f c acc
-              | c == '\'' = acc
-              | C.isPunctuation c = ' ':acc
-              | otherwise = c:acc
-    abbrevAllCaps xs = if isAllCaps xs then take 1 xs else xs
-
+abbreviate :: Text -> Text
+abbreviate = T.toUpper . T.filter C.isAlpha . snd . T.mapAccumL dispatch ' '
+  where dispatch prev c
+          | not $ C.isAlpha c = (c, ' ')
+          | prev /= '\'' && (C.isSpace prev || C.isPunctuation prev) = (c, c)
+          | C.isLower prev && C.isUpper c = (c, c)
+          | otherwise = (c, ' ')
