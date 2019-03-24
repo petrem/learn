f2cbbc8792f4bf76e8ca9c64738c09acb176bc64
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-03-24 20:11:58 +0200

    exercism: c: update isogram extracting magic number of letters to a const

--- Exercism/c/isogram/src/isogram.c
+++ Exercism/c/isogram/src/isogram.c
@@ -3,15 +3,17 @@
 #include <string.h>
 #include "isogram.h"
 
+static const unsigned int NUM_LETTERS = 26;
+
 bool is_isogram(const char phrase[]) {
-  uint8_t occurences[26];
+  uint8_t occurences[NUM_LETTERS];
   char *c;
   int idx;
 
   if (phrase == NULL)
     return false;
 
-  memset(occurences, 0, 26);
+  memset(occurences, 0, NUM_LETTERS);
   for (c = (char *)phrase; *c; c++) {
     if (isalpha(*c)) {
       idx = tolower(*c) - 'a';
