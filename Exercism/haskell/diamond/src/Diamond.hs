09c4051ab56d6b9b19fa823e991beb6e55991fc5
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-03-29 22:44:41 +0200

    exercism:haskell:diamond

--- Exercism/haskell/diamond/src/Diamond.hs
+++ Exercism/haskell/diamond/src/Diamond.hs
@@ -1,4 +1,21 @@
 module Diamond (diamond) where
 
+
+import qualified Data.Char as C
+
+
 diamond :: Char -> Maybe [String]
-diamond = error "You need to implement this function"
+diamond c
+  | not (C.isAsciiUpper c) = Nothing
+  | otherwise = Just (_diamond c)
+
+_diamond :: Char -> [String]
+_diamond letter = [lineOf c | c <- ['A'..pred letter] ++ [letter,pred letter..'A']]
+  where
+    offset c = C.ord c - C.ord 'A'
+    displacement c = offset letter - offset c
+    wing c = replicate (displacement c) ' '
+    middle c
+      | c == 'A' = "A"
+      | otherwise = [c] ++ (replicate (2 * offset c - 1) ' ') ++ [c]
+    lineOf c = (wing c) ++ (middle c) ++ (wing c)
