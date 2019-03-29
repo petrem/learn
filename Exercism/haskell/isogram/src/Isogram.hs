f9f04d31bddfdac56c10038e3073bf70686be8e4
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-03-29 07:17:37 +0200

    exercism:haskell:isogram Text version

--- Exercism/haskell/isogram/src/Isogram.hs
+++ Exercism/haskell/isogram/src/Isogram.hs
@@ -1,10 +1,13 @@
 module Isogram (isIsogram) where
 
+{-# LANGUAGE OverloadedStrings #-}
+
+
 import qualified Data.Char as C
 import qualified Data.Set  as S
+import qualified Data.Text as T
+import           Data.Text (Text)
 
-isIsogram :: String -> Bool
-isIsogram =  areUnique . (filter C.isAlphaNum) . (map C.toLower)
-
-areUnique :: String -> Bool
-areUnique = (==) <$> (length . id) <*> (length . S.fromList)
+isIsogram :: Text -> Bool
+isIsogram = snd . (T.foldl helper (S.empty, True)) . T.toLower . (T.filter C.isAlphaNum)
+  where helper acc x = (S.insert x (fst acc), (snd acc) && not (S.member x (fst acc)))
