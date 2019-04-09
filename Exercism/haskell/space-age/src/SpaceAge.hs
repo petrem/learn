cfd13c02be1c0e59cae2675c038f767cedc596c1
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-04-09 08:46:28 +0300

    exercism:haskell:space-age

---
+++ Exercism/haskell/space-age/src/SpaceAge.hs
@@ -0,0 +1,36 @@
+module SpaceAge (Planet(..), ageOn) where
+
+data Planet = Mercury
+            | Venus
+            | Earth
+            | Mars
+            | Jupiter
+            | Saturn
+            | Uranus
+            | Neptune deriving (Eq, Ord, Show)
+
+
+relativeOrbitalPeriod :: Planet -> Double
+relativeOrbitalPeriod Earth = 1
+relativeOrbitalPeriod Mercury = 0.2408467
+relativeOrbitalPeriod Venus = 0.61519726
+relativeOrbitalPeriod Mars = 1.8808158
+relativeOrbitalPeriod Jupiter = 11.862615
+relativeOrbitalPeriod Saturn = 29.447498
+relativeOrbitalPeriod Uranus = 84.016846
+relativeOrbitalPeriod Neptune = 164.79132
+
+earthYear :: Double
+earthYear = 31557600
+
+
+-- An unbiased, "human-like" round, as most of us would expect round to work
+-- e.g. roundHalfAwayFromZero 1.525 2 == 1.53
+-- but  roundTo 2 1.525 == 1.52
+--        where roundTo n = (/ 10 ^ n) . fromIntegral . round . (* 10 ^ n)
+
+roundHalfAwayFromZero :: Double -> Int -> Double
+roundHalfAwayFromZero x = (/) <$> fromInteger . floor . (+ 0.5) . (* x) . (10 ^^) <*> (10 ^^)
+
+ageOn :: Planet -> Double -> Double
+ageOn planet = flip roundHalfAwayFromZero 2 . (/ relativeOrbitalPeriod planet) . (/ earthYear)
