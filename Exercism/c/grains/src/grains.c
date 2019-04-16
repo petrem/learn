1412308216abff152c43fde2a4a3965c253909ed
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-04-16 06:50:46 +0300

    added illuminating comments to grains.c

--- Exercism/c/grains/src/grains.c
+++ Exercism/c/grains/src/grains.c
@@ -2,12 +2,14 @@
 #include <stdint.h>
 
 uint64_t square(unsigned int n) {
+  /* `(n-1) & 0x40` should just be `n > 64`. Just fun with bits */
   if (n == 0 || ((n - 1) & 0x40) != 0) {
     return 0;
   }
   return 1ull << (n - 1);
 }
 
+/* This could return a constat, of course */
 uint64_t total() {
   uint64_t total = 0;
   for(unsigned int i = 1; i <= 64; i++) {
