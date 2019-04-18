401fe5d505b43248a70797c011bb69004362780f
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-04-18 13:34:00 +0300

    eliminate redundant ()

--- Exercism/haskell/pangram/src/Pangram.hs
+++ Exercism/haskell/pangram/src/Pangram.hs
@@ -4,4 +4,4 @@ import Data.Char (toLower, isAlpha)
 import Data.List (nub, sort)
 
 isPangram :: String -> Bool
-isPangram = (26 == ) . length . nub . sort . (map toLower) . filter isAlpha
+isPangram = (26 == ) . length . nub . sort . map toLower . filter isAlpha
