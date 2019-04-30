00e7f875cc0258e60bfaa89add76c6f674df6295
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-04-30 21:39:37 +0300

    exercism:python:bob simplified as isupper ignores non-alpha

--- Exercism/python/bob/bob.py
+++ Exercism/python/bob/bob.py
@@ -1,28 +1,18 @@
-import string
-
-
 def is_question(phrase):
     return phrase.endswith("?")
 
 
 def is_shout(phrase):
-    letters = list(filter(str.isalpha, phrase))
-    if letters:
-        print("letters", letters)
-        return all(c.isupper() for c in letters)
-    return False
+    # I just learned isupper() ignores non-alpha characters -- weird!
+    return phrase.isupper()
 
 
 def is_silence(phrase):
     return phrase == ""
 
 
-def remove_whitespace(s):
-    return s.translate(str.maketrans("", "", string.whitespace))
-
-
 def hey(phrase):
-    cooked_phrase = remove_whitespace(phrase)
+    cooked_phrase = phrase.strip()
     if is_silence(cooked_phrase):
         return "Fine. Be that way!"
     if is_shout(cooked_phrase):
