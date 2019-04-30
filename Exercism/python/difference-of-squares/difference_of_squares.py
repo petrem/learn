d7273d716b823c0ab0a5b4d8981844a99ef6b3fb
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-04-30 15:32:32 +0300

    exercism:python:difference-of-squares copied a solution that does the math, for future reference

--- Exercism/python/difference-of-squares/difference_of_squares.py
+++ Exercism/python/difference-of-squares/difference_of_squares.py
@@ -6,10 +6,6 @@ def sum_of_squares(count):
     return sum(i ** 2 for i in range(1, count + 1))
 
 
-def difference1(count):
-    return square_of_sum(count) - sum_of_squares(count)
-
-
 def difference(count):
     _sum = 0
     half_diff = 0
@@ -17,3 +13,17 @@ def difference(count):
         _sum += i
         half_diff += _sum * (i + 1)
     return 2 * half_diff
+
+
+# solutions from https://exercism.io/tracks/python/exercises/difference-of-squares/solutions/272b31053b344b42b213f05635b34ff3
+
+def square_of_sum1(count):
+    return ((count+1)*count/2)**2
+
+
+def sum_of_squares1(count):
+    return (count*(count+1)*(2*count+1))/6
+
+
+def difference1(count):
+    return square_of_sum1(count)-sum_of_squares1(count)
