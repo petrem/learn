c6f7e4eea3239b83a94bcccecc9bee08a33f8892
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-05-01 11:08:15 +0300

    exercism:python:perfect-numbers

--- Exercism/python/perfect-numbers/perfect_numbers.py
+++ Exercism/python/perfect-numbers/perfect_numbers.py
@@ -1,9 +1,13 @@
+def cmp(x, y):
+    return (x > y) - (y > x)
+
+
 def classify(number):
     if number <= 0:
         raise ValueError("Cannot classify negative or zero number")
-    aliquot_sum = sum(x for x in range(1, number - 1) if number % x == 0)
-    if aliquot_sum == number:
-        return "perfect"
-    if aliquot_sum < number:
-        return "deficient"
-    return "abundant"
+    return ["perfect", "abundant", "deficient"][
+        cmp(
+            sum(x for x in range(1, number - 1) if number % x == 0),
+            number
+        )
+    ]
