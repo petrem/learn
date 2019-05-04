e8d05b7371dca117507e9fec5de7afa937887239
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-05-04 10:25:54 +0300

    exercism:c:difference-of-squares with formulas

--- Exercism/c/difference-of-squares/src/difference_of_squares.c
+++ Exercism/c/difference-of-squares/src/difference_of_squares.c
@@ -1,6 +1,8 @@
 #include "difference_of_squares.h"
 #include <stdio.h>
 
+/* Original versions, where I was lazy to look up or come up with a formula:
+
 long square_of_sum(long n) {
   long sum = 0;
   for (; n>0; n--) {
@@ -16,7 +18,29 @@ long sum_of_squares(long n) {
   }
   return sum;
 }
+*/
 
+long square_of_sum(long n) {
+  long sum = n * (n + 1) / 2;
+  return sum * sum;
+}
+
+long sum_of_squares(long n) {
+  return n * (n + 1) * (2 * n + 1) / 6;
+}
+
+/* The following uses a self-concocted formula. In the current, O(1) versions
+ * of the functions above, it would be faster to implement it as
+ * return square_of_sum(n) - sum_of_squares(n);
+ *
+ * This is easy to calculate:
+ * (1+2+3)*(1+2+3) - 1*1 - 2*2 - 3*3 =
+ * 1*1 + 1*2 + 1*3 + 2*1 + 2*2 + 2*3 + 3*1 + 3*2 + 3*3 - 1*1 - 2*2 - 3*3 =
+ * 1*2 + 1*3 + 2*1 +  2*3 + 3*1 + 3*2 = (1*3 + 2*3 + 3*1 + 3*2) + (1*2 + 2*1)
+ * 2 * [3 * (1+2) + 2 * (1)]
+ * So half of it is the sum of n*(sum 1..n-1). It is easier to see if you do the above
+ * for higher n, e.g. 4 instead of 3.
+ */
 
 long difference_of_squares(long n) {
   long sum = 0;
