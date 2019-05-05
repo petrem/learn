1f734497dd0eabe332af3f1eefc9c7aaf5f95704
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-05-05 10:50:39 +0300

    exercism:haskell:pascals-triangle

---
+++ Exercism/haskell/pascals-triangle/src/Triangle.hs
@@ -0,0 +1,9 @@
+module Triangle (rows) where
+
+rows :: Int -> [[Integer]]
+rows n = [row i | i <- [0..(n-1)]]
+
+row :: Int -> [Integer]
+row 0 = [1]
+row n = 1:middle ++ [1] where
+  middle = map (uncurry (+)) . (zip <$> take (n - 1) <*> drop 1) $ row (n - 1)
