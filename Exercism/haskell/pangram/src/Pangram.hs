218591c45bdee2f9dedc23f29885ef8f5562abea
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-05-21 05:13:59 +0300

    exercism:haskell:pangram add benchmark

--- Exercism/haskell/pangram/src/Pangram.hs
+++ Exercism/haskell/pangram/src/Pangram.hs
@@ -4,6 +4,11 @@ import Data.Char (toLower, isAlpha)
 import Data.List (group, nub, sort)
 import qualified Data.Set as Set
 
+import Control.Exception
+import Control.Monad
+import System.Time.Extra
+
+
 isPangram1 :: String -> Bool
 isPangram1 = (26 == ) . length . nub . sort . map toLower . filter isAlpha
 
@@ -15,3 +20,15 @@ isPangram2 = (26 == ) . length . (map head . group) . sort . map toLower . filte
 -- likewise, using Sets
 isPangram :: String -> Bool
 isPangram = (26 == ) . Set.size . Set.fromList . map toLower . filter isAlpha
+
+
+
+benchmark xs = do
+    n <- evaluate $ length xs
+    (t1,_) <- duration $ evaluate $ isPangram xs
+    (t2,_) <- duration $ evaluate $ isPangram2 xs
+    putStrLn $ show n ++ "," ++ show t1 ++ "," ++ show t2
+
+main = do
+    forM_ [0,100..10000] $ \i -> benchmark $ take (26 * i) $ cycle ['a'..'z']
+    forM_ [0,100..10000] $ \i -> benchmark $ replicate i 'a'
