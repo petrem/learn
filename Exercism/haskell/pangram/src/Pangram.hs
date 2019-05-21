9e0f9542ee1731e82da2dcd083fc32a11b86fee0
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-05-21 04:31:38 +0300

    exercism:haskell:pangram two ways to get rid of nub

--- Exercism/haskell/pangram/src/Pangram.hs
+++ Exercism/haskell/pangram/src/Pangram.hs
@@ -1,7 +1,17 @@
 module Pangram (isPangram) where
 
 import Data.Char (toLower, isAlpha)
-import Data.List (nub, sort)
+import Data.List (group, nub, sort)
+import qualified Data.Set as Set
 
+isPangram1 :: String -> Bool
+isPangram1 = (26 == ) . length . nub . sort . map toLower . filter isAlpha
+
+-- getting rid of nub, need only to remove successive duplicates
+isPangram2 :: String -> Bool
+isPangram2 = (26 == ) . length . (map head . group) . sort . map toLower . filter isAlpha
+
+
+-- likewise, using Sets
 isPangram :: String -> Bool
-isPangram = (26 == ) . length . nub . sort . map toLower . filter isAlpha
+isPangram = (26 == ) . Set.size . Set.fromList . map toLower . filter isAlpha
