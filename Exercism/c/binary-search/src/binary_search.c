05e5eda1865e73d4e03a9e8a9ff9e690c7936541
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-05-26 12:00:48 +0300

    exercism:c:binary-search changes as per track request, not all to my liking

--- Exercism/c/binary-search/src/binary_search.c
+++ Exercism/c/binary-search/src/binary_search.c
@@ -2,27 +2,20 @@
 #include <stddef.h>
 
 int *binary_search(int needle, int haystack[], size_t length) {
-  if (haystack == NULL || length == 0) {
-    return NULL;
-  }
-  size_t left=0, right = length - 1;
-  while (left < right) {
-    size_t middle = left + ((right - left) >> 1);
-    if (needle < haystack[middle]) {
-      if (middle == 0) {
-        break;
+  if (haystack != NULL) {
+    int left = 0, right = length - 1;
+    while (left <= right) {
+      int middle = left + ((right - left) / 2);
+      if (needle < haystack[middle]) {
+        right = middle - 1;
       }
-      right = middle - 1;
+      else if (needle > haystack[middle]) {
+        left = middle + 1;
+      }
+      else {
+        return &haystack[middle];
+      }
     }
-    else if (needle > haystack[middle]) {
-      left = middle + 1;
-    }
-    else {
-      return &haystack[middle];
-    }
-  }
-  if (left == right && needle == haystack[left]) {
-    return &haystack[left];
   }
   return NULL;
 }
