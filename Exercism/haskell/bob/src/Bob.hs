372b0924ca32b05c17094c8f5a595ab37e4c5596
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-06-06 22:46:43 +0300

    exercism:haskell:bob

---
+++ Exercism/haskell/bob/src/Bob.hs
@@ -0,0 +1,25 @@
+module Bob (responseFor) where
+
+{-# LANGUAGE OverloadedStrings #-}
+
+import qualified Data.Char as C
+import qualified Data.Text as T
+import           Data.Text (Text)
+
+
+isQuestion :: Text -> Bool
+isQuestion = T.isSuffixOf "?" . T.stripEnd
+
+isShout :: Text -> Bool
+isShout = (&&) <$> T.any C.isUpper <*> not . T.any ((&&) <$> C.isLetter <*> C.isLower)
+
+isSilence :: Text -> Bool
+isSilence = T.all C.isSpace
+
+responseFor :: Text -> Text
+responseFor xs
+  | (&&) <$> isQuestion <*> isShout $ xs = "Calm down, I know what I'm doing!"
+  | isQuestion xs = "Sure."
+  | isShout xs = "Whoa, chill out!"
+  | isSilence xs = "Fine. Be that way!"
+  | otherwise = "Whatever."
