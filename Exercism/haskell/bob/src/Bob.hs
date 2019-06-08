89b9ca0bcebe0ba736ff1a8aa8cdbb14f18f7fde
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-06-08 17:52:33 +0300

    exercism:haskell:bob use operator and reorder functions

--- Exercism/haskell/bob/src/Bob.hs
+++ Exercism/haskell/bob/src/Bob.hs
@@ -7,19 +7,23 @@ import qualified Data.Text as T
 import           Data.Text (Text)
 
 
-isQuestion :: Text -> Bool
-isQuestion = T.isSuffixOf "?" . T.stripEnd
-
-isShout :: Text -> Bool
-isShout = (&&) <$> T.any C.isUpper <*> not . T.any ((&&) <$> C.isLetter <*> C.isLower)
-
-isSilence :: Text -> Bool
-isSilence = T.all C.isSpace
-
 responseFor :: Text -> Text
 responseFor xs
-  | (&&) <$> isQuestion <*> isShout $ xs = "Calm down, I know what I'm doing!"
+  | isQuestion .&&. isShout $ xs = "Calm down, I know what I'm doing!"
   | isQuestion xs = "Sure."
   | isShout xs = "Whoa, chill out!"
   | isSilence xs = "Fine. Be that way!"
   | otherwise = "Whatever."
+
+infixr 3 .&&.
+(.&&.) :: (a -> Bool) -> (a -> Bool) -> a -> Bool
+p1 .&&. p2 = (&&) <$> p1 <*> p2
+
+isQuestion :: Text -> Bool
+isQuestion = T.isSuffixOf "?" . T.stripEnd
+
+isShout :: Text -> Bool
+isShout = T.any C.isUpper .&&. not . T.any (C.isLetter .&&. C.isLower)
+
+isSilence :: Text -> Bool
+isSilence = T.all C.isSpace
