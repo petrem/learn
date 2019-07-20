474eccd5d8b51b05886310ba9fa14edd2b053329
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-07-20 09:07:26 +0300

    exercism:haskell:beer-song

---
+++ Exercism/haskell/beer-song/src/Beer.hs
@@ -0,0 +1,38 @@
+module Beer (song) where
+
+import Data.Char (toUpper)
+import Data.List (intercalate)
+--import Text.Printf (printf)
+
+
+song :: String
+song = intercalate "\n" $ map stanza [99,98..0]
+
+stanza :: Int -> String
+stanza = (++) <$> verseOne <*> verseTwo
+  where verseOne = versify <$> capitalize . bottlesOnTheWall <*> bottlesOfBeer
+        verseTwo = versify <$> obtainBeer <*> bottlesOnTheWall . oneLess
+        capitalize = (:) <$> toUpper . head <*> tail
+        oneLess n
+          | n == 0    = 99
+          | otherwise = n - 1
+
+versify :: String -> String -> String
+versify part1 part2 = (part1 ++ ", ") ++ (part2 ++ ".\n")
+
+obtainBeer :: Int -> String
+obtainBeer n
+  | n == 0    = "Go to the store and buy some more"
+  | otherwise = let what = if n == 1 then "it" else "one" in
+                  "Take " ++ what ++ " down and pass it around"
+
+bottlesOnTheWall :: Int -> String
+bottlesOnTheWall = (++ " on the wall") . bottlesOfBeer
+
+
+bottlesOfBeer :: Int -> String
+bottlesOfBeer n = howMany ++ " bottle" ++ plural ++ " of beer"
+  where howMany
+          | n >= 1    = show n
+          | otherwise = "no more"
+        plural = if n /= 1 then "s" else ""
