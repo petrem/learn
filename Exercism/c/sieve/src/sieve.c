faf856e8a2edff26accb07a2112ae3490d17b9e4
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-08-07 10:32:48 +0300

    exercism:c:sieve optimized version (but still Erathostenes sieve)

--- Exercism/c/sieve/src/sieve.c
+++ Exercism/c/sieve/src/sieve.c
@@ -2,20 +2,30 @@
 #include <stddef.h>
 #include <string.h>
 
+
 #define UNMARKED  (char)0
 #define NOT_PRIME (char)1
+
 
 uint32_t sieve(uint32_t limit, uint32_t *primes, size_t max_primes) {
   uint32_t n_primes = 0;
   char marks[limit + 1];
+  uint32_t i = 2;
   memset(marks, UNMARKED, limit + 1);
-  for (uint32_t i = 2; i <= limit; i++) {
+  for (; i * i <= limit; i++) {
     if (marks[i] == UNMARKED) {
       primes[n_primes++] = i;
       if (n_primes == max_primes)
         break;
-      for (uint32_t j = i+i; j <= limit ; j += i)
+      for (uint32_t j = i * i; j <= limit ; j += i)
         marks[j] = NOT_PRIME;
+    }
+  }
+  for (; i <= limit; i++) {
+    if (marks[i] == UNMARKED) {
+      primes[n_primes++] = i;
+      if (n_primes == max_primes)
+        break;
     }
   }
   return n_primes;
