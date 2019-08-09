432fa79933511098475887a29972d9eec181eda2
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-08-09 19:39:46 +0300

    exercism:python:sieve

---
+++ Exercism/python/sieve/sieve.py
@@ -0,0 +1,46 @@
+from array import array
+from itertools import repeat
+from math import sqrt, trunc
+
+
+def primes(limit):
+    primes = []
+    marks = BitArray(limit + 1)
+    mark_until = trunc(sqrt(limit))
+    for i in range(2, mark_until + 1):
+        if marks[i] == 0:
+            primes.append(i)
+            for j in range(i * i, limit + 1, i):
+                marks[j] = 1
+
+    for i in range(mark_until + 1, limit + 1):
+        if marks[i] == 0:
+            primes.append(i)
+    return primes
+
+
+class BitArray():
+    WORDTYPE = "L"
+    WORDSIZE = 8 * 8
+    WORDMASK = WORDSIZE - 1
+
+    def __init__(self, size):
+        self._size = size
+        self._words = array(
+            BitArray.WORDTYPE,
+            repeat(0, (size + BitArray.WORDSIZE - 1) // BitArray.WORDSIZE)
+        )
+
+    def __getitem__(self, index):
+        word = self._words[index // BitArray.WORDSIZE]
+        return (word >> (index & BitArray.WORDMASK)) & 1
+
+    def __setitem__(self, index, value):
+        word_index = index // BitArray.WORDSIZE
+        shift = index & BitArray.WORDMASK
+        word = self._words[word_index]
+        if (word >> shift) & 1 != value:
+            self._words[word_index] = word ^ (1 << shift)
+
+    def __str__(self):
+        return "[" + ", ".join(str(self[i]) for i in range(0, self._size + 1)) + "]"
