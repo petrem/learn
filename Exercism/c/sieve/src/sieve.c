647a1aab57f5525b93f454b57ce0fca7662dca41
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-08-09 18:33:53 +0300

    exercism:c:sieve use a bitvector to reduce memory footprint

--- Exercism/c/sieve/src/sieve.c
+++ Exercism/c/sieve/src/sieve.c
@@ -1,32 +1,65 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
 
 
-#define UNMARKED  (char)0
-#define NOT_PRIME (char)1
+#define WORDTYPE uint_fast32_t
+#define WORDSIZE sizeof(WORDTYPE)
+#define WORDMASK (WORDTYPE) WORDSIZE - 1
+
+#define UNMARKED  (unsigned) 0
+#define NOT_PRIME (unsigned) 1
+
+static WORDTYPE *bitvector_init(uint32_t limit);
+static inline void bitvector_set(WORDTYPE *vector, uint32_t index, unsigned value);
+static inline unsigned bitvector_get(WORDTYPE *vector, uint32_t index);
 
 
 uint32_t sieve(uint32_t limit, uint32_t *primes, size_t max_primes) {
-  uint32_t n_primes = 0;
-  char marks[limit + 1];
+  size_t n_primes = 0;
+  WORDTYPE *marks = bitvector_init(limit + 1);
   uint32_t i = 2;
-  memset(marks, UNMARKED, limit + 1);
   for (; i * i <= limit; i++) {
-    if (marks[i] == UNMARKED) {
+    if (bitvector_get(marks, i) == UNMARKED) {
       primes[n_primes++] = i;
       if (n_primes == max_primes)
         break;
       for (uint32_t j = i * i; j <= limit ; j += i)
-        marks[j] = NOT_PRIME;
+        bitvector_set(marks, j, NOT_PRIME);
     }
   }
   for (; i <= limit; i++) {
-    if (marks[i] == UNMARKED) {
+    if (bitvector_get(marks, i) == UNMARKED) {
       primes[n_primes++] = i;
       if (n_primes == max_primes)
         break;
     }
   }
   return n_primes;
+}
+
+
+static WORDTYPE *bitvector_init(uint32_t limit) {
+  WORDTYPE *data = calloc((limit + WORDSIZE - 1) / WORDSIZE, WORDSIZE);
+  if (data == NULL) {
+    fputs("Could not allocate memory. Good bye!", stderr);
+    exit(ENOMEM);
+  }
+  return data;
+}
+
+static inline void bitvector_set(WORDTYPE *vector, uint32_t index, unsigned value) {
+  size_t word_index = index / WORDSIZE;
+  size_t shift = index & WORDMASK;
+  WORDTYPE word = vector[word_index];
+  if (((word >> shift) & 1) != value) {
+    vector[word_index] = word ^ (1 << shift);
+  }
+}
+
+static inline unsigned bitvector_get(WORDTYPE *vector, uint32_t index) {
+  return (vector[index / WORDSIZE] >> (index & WORDMASK)) & 1;
 }
