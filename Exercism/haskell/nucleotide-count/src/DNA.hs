f36171f3d82e6bcd300b3db68b63f9526dc13d90
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-08-15 19:07:07 +0300

    exercism:haskell:nucleotide-count type alias

--- Exercism/haskell/nucleotide-count/src/DNA.hs
+++ Exercism/haskell/nucleotide-count/src/DNA.hs
@@ -5,15 +5,14 @@ import Data.Map (Map)
 import qualified Data.Map.Strict as Map
 import Text.Read (readEither)
 
-
 data Nucleotide = A | C | G | T deriving (Eq, Ord, Read, Show)
+type NucleotideCounts = Map Nucleotide Int
 
-
-nucleotideCounts :: String -> Either String (Map Nucleotide Int)
+nucleotideCounts :: String -> Either String NucleotideCounts
 nucleotideCounts = foldl updateCounts (Right zeroCounts)
 
-zeroCounts :: Map Nucleotide Int
+zeroCounts :: NucleotideCounts
 zeroCounts = Map.fromList [(A,0), (C,0), (G,0), (T,0)]
 
-updateCounts :: Either String (Map Nucleotide Int) -> Char -> Either String (Map Nucleotide Int)
+updateCounts :: Either String NucleotideCounts -> Char -> Either String NucleotideCounts
 updateCounts m c = liftA3 (Map.insertWith (+)) (readEither [c]) (Right 1) m
