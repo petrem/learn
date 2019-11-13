ad020e2fba72ea14b7c339af609bf794d7da87fc
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-11-13 20:06:40 +0200

    exercism:javascript:pangram more solutions

--- Exercism/javascript/pangram/pangram.js
+++ Exercism/javascript/pangram/pangram.js
@@ -1,5 +1,26 @@
+"use strict";
+
+const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
 export const isPangram = (thing) => {
+    let lowerThing = thing.toLowerCase();
+    return [...alphabet].every((chr) => lowerThing.includes(chr));
+};
+
+
+// Other solutions
+export const isPangram1 = (thing) => {
     let checklist = new Set("abcdefghijklmnopqrstuvwxyz");
     [...thing.toLowerCase()].forEach( (item, index, array) => { checklist.delete(item); } );
     return checklist.size == 0;
 };
+
+export const isPangram2 = (thing) => {
+    let checklist = new Set();
+    [...thing.toLowerCase()]
+        .filter((chr) => /[a-z]/.test(chr))
+        .reduce((acc, chr) => acc.add(chr), checklist );
+    return checklist.size == 26;
+};
+
+
