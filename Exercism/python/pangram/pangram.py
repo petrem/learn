4f95708d674426a376e4e55aea98504bb64e5bae
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-11-14 08:14:50 +0200

    exercism:python:pangram different solution

--- Exercism/python/pangram/pangram.py
+++ Exercism/python/pangram/pangram.py
@@ -7,3 +7,9 @@ alphabet = set(string.ascii_lowercase)
 def is_pangram(sentence):
     letters = {letter.lower() for letter in sentence if letter.lower() in alphabet}
     return letters == alphabet
+
+
+# the obligatory standard solution
+def is_pangram2(sentence):
+    lower_sentence = sentence.lower()
+    return all(letter in lower_sentence for letter in alphabet)
