c3de9f5670f5705ee871301f4338ca16bf984304
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-11-24 19:52:59 +0200

    javascript.info:arrays:filter_range

--- javascript.info/arrays/filter_range.js
+++ javascript.info/arrays/filter_range.js
@@ -8,3 +8,22 @@
 // alert( arr ); // 5,3,8,1 (not modified)
 
 const filterRange = (arr, min, max) => arr.filter((x) => x >= min && x <= max);
+
+// Write a function filterRangeInPlace(arr, a, b) that gets an array arr and removes from it all values except those that are between a and b. The test is: a ≤ arr[i] ≤ b.
+// The function should only modify the array. It should not return anything.
+// For instance:
+// let arr = [5, 3, 8, 1];
+// filterRangeInPlace(arr, 1, 4); // removed the numbers except from 1 to 4
+// alert( arr ); // [3, 1]
+
+const filterRangeInPlace = (arr, min, max) => {
+    arr.forEach((item, index, array) => {
+        if (item < min || item > max)
+            array.splice(index, 1);
+    });
+};
+
+
+let arr = [5, 3, 8, 1];
+filterRangeInPlace(arr, 1, 4);
+console.log(arr);
