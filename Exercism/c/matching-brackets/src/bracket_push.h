041e3329d2a1cef623b15aeab5648e39a4832c1b
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-12-07 12:28:15 +0200

    exercism:c:matching-brackets update filenames after exercise update

--- Exercism/c/matching-brackets/src/bracket_push.h
+++
@@ -1,8 +0,0 @@
-#ifndef _BRAKET_PUSH_H
-#define _BRAKET_PUSH_H
-
-#include <stdbool.h>
-
-bool is_paired(const char *brackets);
-
-#endif
--- Exercism/c/matching-brackets/src/matching_brackets.h
+++ Exercism/c/matching-brackets/src/matching_brackets.h
@@ -1,8 +1,8 @@
-#ifndef MATCHING_BRACKETS_H
-#define MATCHING_BRACKETS_H
+#ifndef _BRAKET_PUSH_H
+#define _BRAKET_PUSH_H
 
 #include <stdbool.h>
 
-bool is_paired(const char *input);
+bool is_paired(const char *brackets);
 
 #endif
