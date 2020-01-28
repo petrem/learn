c67549ed07740c51a917ac3d1efad7f3c9bc8e9a
Petre Mierlutiu <petrem@users.noreply.github.com>
2020-01-28 09:56:02 +0200

    exercism:python:roman-numerals prepare dict

--- Exercism/python/roman-numerals/roman_numerals.py
+++ Exercism/python/roman-numerals/roman_numerals.py
@@ -2,15 +2,17 @@ def roman(number):
     return "".join(
         reversed(
             [
-                rds[int(a)] for a, rds in zip(reversed(str(number)), roman_digits)
+                rds[d] for d, rds in zip(reversed(str(number)), roman_digits)
             ]
         )
     )
 
 
 roman_digits = [
-    ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
-    ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
-    ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
-    ["M" * i for i in range(10)],
+    {str(i): rd for i, rd in enumerate(rds)} for rds in [
+        ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"],
+        ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"],
+        ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"],
+        ["M" * i for i in range(10)],
+    ]
 ]
