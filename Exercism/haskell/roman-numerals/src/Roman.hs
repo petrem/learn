7019107dbe104f4b21b5ea7f9ced1ae09ed3cbab
Petre Mierlutiu <petrem@users.noreply.github.com>
2020-01-29 13:21:43 +0200

    exercism:haskell:roman-numerals

--- Exercism/haskell/roman-numerals/src/Roman.hs
+++ Exercism/haskell/roman-numerals/src/Roman.hs
@@ -2,13 +2,19 @@ module Roman (numerals) where
 
 import Data.Char (digitToInt)
 
---numerals :: Integer -> Maybe String
-numerals n = Just . concat $ zipWith (\a rs -> rs !! digitToInt a) (reverse . show $ n) romanDigits
+numerals :: Integer -> Maybe String
+numerals n
+  | n <= 0 || n > 3999 = Nothing
+  | otherwise          = Just $ getRomanDigits
+  where
+    arabicDigits = show n
+    getRomanDigit d rds = rds !! digitToInt d
+    getRomanDigits = concat $ reverse $ zipWith getRomanDigit (reverse arabicDigits) romanDigits
 
 
 romanDigits :: [[String]]
 romanDigits = [["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
               ,["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
               ,["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
-              ,["", "M", "MM", "MMM", "MMMM", "MMMMM", "MMMMMM", "MMMMMMM", "MMMMMMMM", "MMMMMMMMM"]
+              ,take 4 $ iterate (\xs -> xs ++ "M") ""
               ]
