d2da8fe7e9872959c559d7e9b406dc18d4fea1f2
Petre Mierlutiu <petrem@users.noreply.github.com>
2020-12-10 21:35:56 +0200

    exercism:haskell:largest-series-product

--- Exercism/haskell/largest-series-product/src/Series.hs
+++ Exercism/haskell/largest-series-product/src/Series.hs
@@ -6,10 +6,14 @@ import Data.List (tails)
 data Error = InvalidSpan | InvalidDigit Char deriving (Show, Eq)
 
 largestProduct :: Int -> String -> Either Error Integer
-largestProduct size digits =  findLargestProduct =<< traverse eitherDigitToInteger digits
-  where findLargestProducts = map product . filter ((==size) . length) . map (take size) . tails
-        findLargestProduct [] = Left InvalidSpan
-        findLargestProduct xs = Right . maximum . findLargestProducts $ xs
+largestProduct size digits = findLargestProduct =<< traverse eitherDigitToInteger digits
+  where
+    findLargestProduct xs | length xs < size = Left InvalidSpan
+                          | size < 0 = Left InvalidSpan
+                          | size == 0 = Right 1
+                          | null xs = Left InvalidSpan
+                          | otherwise = Right . maximum . products $ xs
+    products = map product . filter ((==size) . length) . map (take size) . tails
 
 eitherDigitToInteger :: Char -> Either Error Integer
 eitherDigitToInteger x | x `elem` ['0'..'9'] = Right . toInteger . digitToInt $ x
