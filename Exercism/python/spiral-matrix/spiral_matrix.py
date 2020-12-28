15c6b32a55aebc9443d54cd0f301eca5d8cb2d3e
Petre Mierlutiu <petrem@users.noreply.github.com>
2020-12-28 13:11:32 +0200

    exercism: python: spiral matrix

--- Exercism/python/spiral-matrix/spiral_matrix.py
+++ Exercism/python/spiral-matrix/spiral_matrix.py
@@ -1,24 +1,42 @@
-from enum import Enum
-from itertools import cycle
-from typing import Iterable, Generator
+from __future__ import annotations
+
+import typing as T
+from itertools import chain, repeat
+from dataclasses import dataclass
 
 
 def spiral_matrix(size):
-    matrix = [[None] * size] * size
-    total_numbers = size * size
-    cycles = total_numbers / 3
-    last = total_numbers % 3
-    
-    for direction, i in zip(directions, range(1, size * size + 1)):
-        pass
+    if size == 0:
+        return []
+    matrix = [[None] * size for _ in range(size)]
+    cursor = XY(0, -1)
+    for value, direction in enumerate(_walk_spiral(size), start=1):
+        cursor += direction
+        matrix[cursor.x][cursor.y] = value
+    return matrix
 
 
-def _triads(n):
-    total = n * n
-    side = n - 1
-    directions = cycle([(0, 1), (1, 0), (0, -1), (-1, 0)])
-    while True:
-        if total > 3 * side:
-            yield next(directions) * side
-            total -= 3 * side
-        
+@dataclass
+class XY():
+    x: int
+    y: int
+
+    def __add__(self: XY, other: XY) -> XY:
+        return XY(self.x + other.x, self.y + other.y)
+
+
+RIGHT = XY(0, 1)
+DOWN = XY(1, 0)
+LEFT = XY(0, -1)
+UP = XY(-1, 0)
+
+
+def _walk_spiral(n: int) -> T.Iterable[XY]:
+    k = n - 1
+    while k > 0:
+        yield from chain(
+            repeat(RIGHT, k + 1), repeat(DOWN, k), repeat(LEFT, k), repeat(UP, k - 1)
+        )
+        k = k - 2
+    if n % 2 == 1:
+        yield RIGHT
